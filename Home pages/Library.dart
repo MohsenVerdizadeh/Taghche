5b4f185ccdbc498b7edcb6935e97deed9010fae4
unknown <MohsenVerdizadehkohi@gmail.com>
2023-06-01 00:44:58 +0330

    complete LibrarPagey

--- Home pages/Library.dart
+++ Home pages/Library.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Library extends StatefulWidget {
   const Library({Key? key}) : super(key: key);
@@ -10,11 +11,154 @@ class Library extends StatefulWidget {
 class _LibraryState extends State<Library> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Text('library'),
-        ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Color(0xFF232323),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TabBar(
+              tabs: [
+                Tab(
+                  text: 'Your books',
+                ),
+                Tab(
+                  text: 'Shelves',
+                ),
+                Tab(
+                  text: 'Series',
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: ListView(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Color(0xFF297171),
+                          ),
+                          child: Center(
+                            child: ListTile(
+                              title: Text(
+                                'Ebooks',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'OoohBaby',
+                                  color: Color(0xFF3dd9d6),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                                color: Color(0xFF3dd9d6),
+                              ),
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Color(0xFF297171),
+                          ),
+                          child: Center(
+                            child: ListTile(
+                              title: Text(
+                                'Audiobooks',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'OoohBaby',
+                                  color: Color(0xFF3dd9d6),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                                color: Color(0xFF3dd9d6),
+                              ),
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Color(0xFF297171),
+                          ),
+                          child: Center(
+                            child: ListTile(
+                              title: Text(
+                                'Downloaded',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'OoohBaby',
+                                  color: Color(0xFF3dd9d6),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                                color: Color(0xFF3dd9d6),
+                              ),
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Color(0xFF297171),
+                          ),
+                          child: Center(
+                            child: ListTile(
+                              title: Text(
+                                'Readed',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'OoohBaby',
+                                  color: Color(0xFF3dd9d6),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                                color: Color(0xFF3dd9d6),
+                              ),
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(),
+                  Container(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
