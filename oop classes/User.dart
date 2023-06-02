5d1a881b9e187881228c4d5bfc123bbc10807f28
unknown <MohsenVerdizadehkohi@gmail.com>
2023-06-02 18:24:09 +0330

    add User class

---
+++ oop classes/User.dart
@@ -0,0 +1,10 @@
+class User {
+  final String name;
+  final String email;
+  final int credit;
+  final String password;
+  final String imageUrl;
+
+  User(this.name, this.email, this.credit, this.password, this.imageUrl);
+
+}
