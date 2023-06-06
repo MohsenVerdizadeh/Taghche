bd5a647892469c46d5d4dd6c886dc02dff0c3616
unknown <MohsenVerdizadehkohi@gmail.com>
2023-06-06 21:44:36 +0330

    add search textFieid

--- Home pages/Shop.dart
+++ Home pages/Shop.dart
@@ -15,18 +15,41 @@ class _ShopState extends State<Shop> {
       backgroundColor: const Color(0xFF232323),
       appBar: AppBar(
         backgroundColor: Color(0xFF297171),
-        title: Text(
-          'Shop',
-          style: TextStyle(
-            fontSize: 25,
-            fontFamily: 'OoohBaby',
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF3dd9d6),
+        title: Center(
+          child: Text(
+            'Shop        ',
+            style: TextStyle(
+              fontSize: 25,
+              fontFamily: 'OoohBaby',
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF3dd9d6),
+            ),
           ),
         ),
       ),
       body: ListView(
+        children: [
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF297171),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      border: InputBorder.none,
+                      hintText: 'Search'),
+                ),
+              ),
+            ),
+          ),
 
+        ],
       ),
     );
   }
