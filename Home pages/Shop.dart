6613c1b3acd3c4e3dd95e2c4000b8e38a99654d5
unknown <MohsenVerdizadehkohi@gmail.com>
2023-06-06 21:37:55 +0330

    little change sign-up.dart

--- Home pages/Shop.dart
+++ Home pages/Shop.dart
@@ -12,7 +12,22 @@ class _ShopState extends State<Shop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: const Color(0xFF232323),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF297171),
+        title: Text(
+          'Shop',
+          style: TextStyle(
+            fontSize: 25,
+            fontFamily: 'OoohBaby',
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF3dd9d6),
+          ),
+        ),
+      ),
+      body: ListView(
+
+      ),
     );
   }
 }
